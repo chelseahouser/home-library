840cacc21ef0329c99a6cbf07e00bc660e56d265
Chelsea Houser <chelsearhouser@gmail.com>
2022-02-27 14:24:06 -0600

    Configuration and Secret Storage

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { BooksModule } from './modules/books/books.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigService } from './config.service';
+import { ConfigModule } from './config.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('connectionString'), BooksModule],
+  imports: [
+    ConfigModule,
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: await configService.getDatabaseConnectionStringConfig(),
+      }),
+      inject: [ConfigService],
+    }),
+    BooksModule,
+  ],
 })
 export class AppModule {}
---
+++ src/config.service.ts
@@ -0,0 +1,38 @@
+import { SecretManagerServiceClient } from '@google-cloud/secret-manager';
+import * as dotenv from 'dotenv';
+
+export class ConfigService {
+  private readonly envConfig: Record<string, string>;
+
+  constructor() {
+    const result = dotenv.config();
+
+    // Save Loaded Configurations
+    if (result.error) {
+      this.envConfig = process.env;
+    } else {
+      this.envConfig = result.parsed;
+    }
+  }
+
+  public get(key: string): string {
+    return this.envConfig[key];
+  }
+
+  public async getDatabaseConnectionStringConfig() {
+    if (this.get('GSM_ACTIVE') == 'true') {
+      return await this.getSecrets('MONGO_DB');
+    }
+    return this.get('MONGO_DB');
+  }
+
+  public async getSecrets(secret: string): Promise<string> {
+    const projectId = this.get('GOOGLE_PROJECT_ID');
+    const secretId = this.get(secret);
+
+    const client = new SecretManagerServiceClient();
+    const versionName = `projects/${projectId}/secrets/${secretId}/versions/latest`;
+    const [version] = await client.accessSecretVersion({ name: versionName });
+    return version.payload.data.toString(); //secret.payload.data.toString('utf8');
+  }
+}
